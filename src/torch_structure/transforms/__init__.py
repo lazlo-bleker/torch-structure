6b4ec3e9b8cec83fd1c1d096d781654857c2590b
lazlo-bleker <lwbleker@gmail.com>
2025-07-02 18:36:59 +0200

    added gaussian z noise transform

--- src/torch_structure/transforms/__init__.py
+++ src/torch_structure/transforms/__init__.py
@@ -1,3 +1,4 @@
-from .laplacian_noise import AddLaplacianZNoise
+from .laplacian_z_noise import AddLaplacianZNoise
+from .gaussian_z_noise import AddGaussianZNoise
 
-__all__ = ['AddLaplacianZNoise']
+__all__ = ['AddLaplacianZNoise', 'AddGaussianZNoise']
---
+++ src/torch_structure/transforms/gaussian_z_noise.py
@@ -0,0 +1,32 @@
+import torch
+from torch_geometric.transforms import BaseTransform
+
+class AddGaussianZNoise(BaseTransform):
+    r"""
+    Applies independent Gaussian multiplicative noise to the z-coordinate of each node.
+
+    Args:
+        std (float): The standard deviation of the noise coefficient.
+
+    Example:
+        >>> transform = AddGaussianZNoise(std=0.1)
+        >>> data = transform(data)
+    """
+    def __init__(self, std=0.1):
+        super().__init__()
+        self.std = std
+
+    def forward(self, data):
+        if not hasattr(data, "coords"):
+            raise AttributeError("Data object has no attribute 'coords'.")
+        
+        device = data.coords.device
+
+        noise = torch.randn(data.num_nodes, device=device) * self.std
+        data.coords[:, 2] = (1 + noise) * data.coords[:, 2]
+        
+        return data
+    
+    def __repr__(self):
+        return (f"{self.__class__.__name__}(max_noise={self.max_noise}, "
+                f"n_eigenvectors={self.n_eigenvectors})")
