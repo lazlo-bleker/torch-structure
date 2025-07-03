61833765a1a55ed2200e7e3e343bcd723c6fde3b
lazlo-bleker <lwbleker@gmail.com>
2025-07-03 13:34:56 +0200

    added data to pytorch safe globals

--- src/torch_structure/data/__init__.py
+++ src/torch_structure/data/__init__.py
@@ -1,5 +1,11 @@
-from .data import StructData
+import torch
+import torch_geometric.typing
+
+from .data import Data, StructData
 from .dataset import Dataset, save, LegacyDataset
 from .view import NodeView
 
-__all__ = ["StructData", "Dataset", "NodeView", "save", "LegacyDataset"]
+__all__ = ["Data", "StructData", "Dataset", "save", "LegacyDataset", "NodeView"]
+
+if torch_geometric.typing.WITH_PT24:
+    torch.serialization.add_safe_globals([Data])
