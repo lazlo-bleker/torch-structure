7f14dbb77a4707c0b68c0f7087850cdef0d1f017
lazlo-bleker <lwbleker@gmail.com>
2025-07-12 14:17:20 +0200

    update torch version check

--- src/torch_structure/data/__init__.py
+++ src/torch_structure/data/__init__.py
@@ -1,5 +1,5 @@
 import torch
-import torch_geometric.typing
+from packaging.version import parse
 
 from .data import Data, StructData
 from .dataset import Dataset, save, LegacyDataset
@@ -7,5 +7,5 @@ from .view import NodeView
 
 __all__ = ["Data", "StructData", "Dataset", "save", "LegacyDataset", "NodeView"]
 
-if torch_geometric.typing.WITH_PT24:
+if parse(torch.__version__) >= parse("2.4.0"):
     torch.serialization.add_safe_globals([Data])
