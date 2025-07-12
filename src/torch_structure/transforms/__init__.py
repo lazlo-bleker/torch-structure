a5641feedc2cb7d6f31fd4184a90a4039798e297
lazlo-bleker <lwbleker@gmail.com>
2025-07-12 13:42:56 +0200

    added z scale transform

--- src/torch_structure/transforms/__init__.py
+++ src/torch_structure/transforms/__init__.py
@@ -1,4 +1,5 @@
 from .laplacian_z_noise import AddLaplacianZNoise
 from .gaussian_z_noise import AddGaussianZNoise
+from .z_scale import ScaleZ
 
-__all__ = ["AddLaplacianZNoise", "AddGaussianZNoise"]
+__all__ = ["AddLaplacianZNoise", "AddGaussianZNoise", "ScaleZ"]
