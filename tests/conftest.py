8a54b81cc4bdc285f0573b6ee66eb15ae6502b0e
lazlo-bleker <lwbleker@gmail.com>
2025-07-30 13:50:45 +0200

    minor fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,14 @@
 import pytest
-from torch_structure.generators.dome import Dome
+from torch_structure.generators.dome import DomeGenerator
 import torch
 import random
 
 
-def dome_graph():  # TODO: Make fully deterministic
-    torch.manual_seed(42)
-    random.seed(42)
-
-    dome = Dome(n_trails=4, n_rings=3, trail_length=1.0, center_deviation_force=0.1)
-    return dome.graph
-
-
 @pytest.fixture
 def dome_data():
-    return dome_graph()
+    torch.manual_seed(42)
+    random.seed(42)
+    
+    generator = DomeGenerator()
+    dome = generator()
+    return dome
