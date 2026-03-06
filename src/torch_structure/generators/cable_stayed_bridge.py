1d0fda1e2f9f39632d65fbb0fc520f2b16f311c5
Daniel <68777522+DanielMezaZ@users.noreply.github.com>
2026-03-06 15:54:15 +0100

    examples updates

--- src/torch_structure/generators/cable_stayed_bridge.py
+++ src/torch_structure/generators/cable_stayed_bridge.py
@@ -1,12 +1,50 @@
-# ToDo: Refactor to inherit from BaseGenerator
-
 import torch
+import numpy as np
 
 from torch_structure.data import StructData
+from torch_structure.generators.base_generator import BaseGenerator
 
 
-class CableStayedBridge:
-    def __init__(
+class CableStayedBridge(BaseGenerator):
+    def __init__(self, **overrides):
+        super().__init__(**overrides)
+        self.max_attempts = 100
+
+        self.node_attrs = {
+            "coords": torch.empty((0, 3), dtype=torch.float),
+            "load": torch.empty((0, 3), dtype=torch.float),
+            "support_condition": torch.empty((0, 3), dtype=torch.long),
+            "is_origin_node": torch.empty((0, 1), dtype=torch.bool),
+            "sequence": torch.empty((0, 1), dtype=torch.long),
+        }
+        self.edge_attrs = {
+            "force": torch.empty((0, 1), dtype=torch.float),
+            "length": torch.empty((0, 1), dtype=torch.float),
+            "is_trail_edge": torch.empty((0, 1), dtype=torch.bool),
+            "force_sign": torch.empty((0, 1), dtype=torch.float),
+        }
+        self.default_attrs = {
+            "force": torch.tensor([torch.nan]),
+            "length": torch.tensor([torch.nan]),
+            "coords": torch.full((3,), torch.nan),
+            "load": torch.zeros(3, dtype=torch.float),
+            "support_condition": torch.zeros(3, dtype=torch.bool),
+            "is_origin_node": torch.tensor(0, dtype=torch.bool),
+        }
+
+    def sample_input(self, **kwargs):
+        """
+        No sampling required
+        """
+        return kwargs
+
+    def validate_input(self, **kwargs):
+        """
+        No validation required
+        """
+        pass
+
+    def generate(
         self,
         n_towers,
         n_cables,
@@ -16,131 +54,98 @@ class CableStayedBridge:
         cable_deviation_force,
         tower_height,
         tower_offset,
-        back_stay_distance,
+        back_stay_offset,
         back_stay_force,
         deck_load=torch.tensor([0.0, 0.0, -1.0]),
     ):
-        # Topological parameters
-        self.n_towers = n_towers
-        self.n_cables = n_cables
-        self.n_deck_trail_edges = self.n_towers * self.n_cables
-
-        # Metric parameters
-        self.deck_trail_length = deck_trail_length
-        self.tower_trail_length = tower_trail_length
-        self.center_deviation_force = center_deviation_force
-        self.cable_deviation_force = cable_deviation_force
-        self.deck_load = deck_load
-        self.tower_height = tower_height
-        self.tower_offset = tower_offset
-        self.back_stay_distance = back_stay_distance
-        self.back_stay_force = back_stay_force
-
-        self.generate_graph()
-
-    def generate_graph(self):
-        node_attrs = {
-            "coords": torch.empty((0, 3), dtype=torch.float),
-            "load": torch.empty((0, 3), dtype=torch.float),
-            "support_condition": torch.empty((0, 3), dtype=torch.long),
-            "is_origin_node": torch.empty((0, 1), dtype=torch.bool),
-            "sequence": torch.empty((0, 1), dtype=torch.long),
-        }
-        edge_attrs = {
-            "force": torch.empty((0, 1), dtype=torch.float),
-            "length": torch.empty((0, 1), dtype=torch.float),
-            "is_trail_edge": torch.empty((0, 1), dtype=torch.bool),
-            "force_sign": torch.empty((0, 1), dtype=torch.float),
-        }
-        default_attrs = {
-            "force": torch.tensor([torch.nan]),
-            "length": torch.tensor([torch.nan]),
-            "coords": torch.full((3,), torch.nan),
-            "load": torch.zeros(3, dtype=torch.float),
-            "support_condition": torch.zeros(3, dtype=torch.bool),
-            "is_origin_node": torch.tensor(0, dtype=torch.bool),
-        }
-        self.graph = StructData(
-            node_attrs=node_attrs, edge_attrs=edge_attrs, default_attrs=default_attrs
+        # Cmpute number of trail edges
+        n_deck_trail_edges = n_towers * n_cables
+        # Initialize graph to contain data
+        graph = StructData(
+            node_attrs=self.node_attrs,
+            edge_attrs=self.edge_attrs,
+            default_attrs=self.default_attrs,
         )
 
         # Deck
         deck_origin_coords = [
-            torch.tensor([-0.5 * self.deck_trail_length, 0.0, 0.0]),
-            torch.tensor([0.5 * self.deck_trail_length, 0.0, 0.0]),
+            torch.tensor([-0.5 * deck_trail_length, 0.0, 0.0]),
+            torch.tensor([0.5 * deck_trail_length, 0.0, 0.0]),
         ]
         for i in range(2):
-            self.graph.add_node(
+            # Add origin nodes
+            graph.add_node(
                 f"deck_trail_{i}_node_0",
                 coords=deck_origin_coords[i],
                 is_origin_node=torch.tensor(True),
                 sequence=torch.tensor(0),
-                load=self.deck_load,
+                load=deck_load,
             )
-            for j in range(1, self.n_deck_trail_edges + 1):
+            for j in range(1, n_deck_trail_edges + 1):
                 load = (
                     torch.zeros(3, dtype=torch.float)
-                    if j == self.n_deck_trail_edges
-                    else self.deck_load
+                    if j == n_deck_trail_edges
+                    else deck_load
                 )
                 support_condition = (
                     torch.tensor([True, True, True])
-                    if j == self.n_deck_trail_edges
+                    if j == n_deck_trail_edges
                     else torch.tensor([False, False, False])
                 )
-                self.graph.add_node(
+                # Add node
+                graph.add_node(
                     f"deck_trail_{i}_node_{j}",
                     is_origin_node=torch.tensor(False),
                     sequence=torch.tensor(j),
                     load=load,
                     support_condition=support_condition,
                 )
-                self.graph.add_edge(
+                # Add new edge with the created node and its previous node
+                graph.add_edge(
                     f"deck_trail_{i}_node_{j - 1}",
                     f"deck_trail_{i}_node_{j}",
                     is_trail_edge=torch.tensor(True),
-                    length=self.deck_trail_length,
+                    length=deck_trail_length,
                     force_sign=torch.tensor(-1.0),
                 )
-        self.graph.add_edge(
+
+        # Add element connecting rails
+        graph.add_edge(
             "deck_trail_0_node_0",
             "deck_trail_1_node_0",
             is_trail_edge=torch.tensor(False),
-            force=self.center_deviation_force,
+            force=center_deviation_force,
         )
 
         # Towers
-        span = (self.n_deck_trail_edges * 2 + 1) * self.deck_trail_length
-        tower_x = torch.linspace(-0.5 * span, 0.5 * span, 2 * self.n_towers + 1)[1:-1:2]
-        for i in range(self.n_towers):
-            tower_offset = (1 - 2 * (i % 2)) * self.tower_offset
+        span = (n_deck_trail_edges * 2 + 1) * deck_trail_length
+        tower_x = torch.linspace(-0.5 * span, 0.5 * span, 2 * n_towers + 1)[1:-1:2]
+        for i in range(n_towers):
+            alternate = 1 - 2 * (i % 2)
+            applied_tower_offset = alternate * tower_offset
             tower_origin_coords = torch.tensor(
-                [tower_x[i], tower_offset, self.tower_height]
+                [tower_x[i], applied_tower_offset, tower_height]
             )
-            self.graph.add_node(
+            graph.add_node(
                 f"tower_trail_{i}_node_0",
                 coords=tower_origin_coords,
                 is_origin_node=torch.tensor(True),
                 sequence=torch.tensor(0),
             )
-            for j in range(1, self.n_cables + 1):
+            for j in range(1, n_cables + 1):
                 support_condition = (
                     torch.tensor([True, True, True])
-                    if j == self.n_cables
+                    if j == n_cables
                     else torch.tensor([False, False, False])
                 )
-                length = (
-                    self.tower_height * 0.8
-                    if j == self.n_cables
-                    else self.tower_trail_length
-                )
-                self.graph.add_node(
+                length = tower_height * 0.8 if j == n_cables else tower_trail_length
+                graph.add_node(
                     f"tower_trail_{i}_node_{j}",
                     is_origin_node=torch.tensor(False),
                     sequence=torch.tensor(j),
                     support_condition=support_condition,
                 )
-                self.graph.add_edge(
+                graph.add_edge(
                     f"tower_trail_{i}_node_{j - 1}",
                     f"tower_trail_{i}_node_{j}",
                     is_trail_edge=torch.tensor(True),
@@ -148,53 +153,53 @@ class CableStayedBridge:
                     force_sign=torch.tensor(-1.0),
                 )
             # Backstay
-            back_stay_offset = (
-                (1 - 2 * (i % 2)) * self.tower_offset * self.back_stay_distance
+            applied_back_stay_offset = alternate * (tower_offset + back_stay_offset)
+            backstay_origin_coords = torch.tensor(
+                [tower_x[i], applied_back_stay_offset, 0.01]
             )
-            backstay_origin_coords = torch.tensor([tower_x[i], back_stay_offset, 0.01])
-            self.graph.add_node(
+            graph.add_node(
                 f"backstay_trail_{i}_node_0",
                 coords=backstay_origin_coords,
                 is_origin_node=torch.tensor(True),
                 sequence=torch.tensor(0),
             )
-            self.graph.add_node(
+            graph.add_node(
                 f"backstay_trail_{i}_node_1",
                 is_origin_node=torch.tensor(False),
                 sequence=torch.tensor(1),
                 support_condition=[True, True, True],
             )
-            self.graph.add_edge(
+            graph.add_edge(
                 f"backstay_trail_{i}_node_0",
                 f"backstay_trail_{i}_node_1",
                 is_trail_edge=torch.tensor(True),
-                length=0.4 * self.tower_height,
+                length=0.4 * tower_height,
                 force_sign=torch.tensor(1.0),
             )
-            self.graph.add_edge(
+            graph.add_edge(
                 f"tower_trail_{i}_node_0",
                 f"backstay_trail_{i}_node_0",
                 is_trail_edge=torch.tensor(False),
-                force=self.back_stay_force,
+                force=back_stay_force,
             )
 
         # Cables
         deck_nodes = [
-            f"deck_trail_0_node_{i}" for i in reversed(range(self.n_deck_trail_edges))
-        ] + [f"deck_trail_1_node_{i}" for i in range(self.n_deck_trail_edges)]
+            f"deck_trail_0_node_{i}" for i in reversed(range(n_deck_trail_edges))
+        ] + [f"deck_trail_1_node_{i}" for i in range(n_deck_trail_edges)]
         cable_nodes = [
             f"tower_trail_{i}_node_{j}"
-            for i in range(self.n_towers)
+            for i in range(n_towers)
             for reverse in [False, True]
-            for j in (
-                reversed(range(self.n_cables)) if reverse else range(self.n_cables)
-            )
+            for j in (reversed(range(n_cables)) if reverse else range(n_cables))
         ]
 
         for deck_node, cable_node in zip(deck_nodes, cable_nodes):
-            self.graph.add_edge(
+            graph.add_edge(
                 deck_node,
                 cable_node,
                 is_trail_edge=torch.tensor(False),
-                force=self.cable_deviation_force,
+                force=cable_deviation_force,
             )
+
+        return graph
---
+++ src/torch_structure/generators/single_trail.py
@@ -0,0 +1,115 @@
+import torch
+import numpy as np
+
+from torch_structure.data import StructData
+from torch_structure.generators.base_generator import BaseGenerator
+
+class SingleTrailGenerator(BaseGenerator):
+    def __init__(self, **overrides):
+        super().__init__(**overrides)
+        self.max_attempts = 1
+
+        self.node_attrs = {
+            "coords": torch.empty((0, 3), dtype=torch.float),
+            "load": torch.empty((0, 3), dtype=torch.float),
+            "support_condition": torch.empty((0, 3), dtype=torch.long),
+            "is_origin_node": torch.empty((0, 1), dtype=torch.bool),
+            "sequence": torch.empty((0, 1), dtype=torch.long),
+        }
+
+        self.edge_attrs = {
+            "force": torch.empty((0, 1), dtype=torch.float),
+            "length": torch.empty((0, 1), dtype=torch.float),
+            "is_trail_edge": torch.empty((0, 1), dtype=torch.bool),
+            "force_sign": torch.empty((0, 1), dtype=torch.float),
+        }
+
+        self.default_attrs = {
+            "coords": torch.full((3,), torch.nan),
+            "load": torch.zeros(3, dtype=torch.float),
+            "support_condition": torch.zeros(3, dtype=torch.bool),
+            "is_origin_node": torch.tensor(0, dtype=torch.bool),
+            "sequence": torch.tensor(-1, dtype=torch.long),
+            "force": torch.tensor([torch.nan]),
+            "length": torch.tensor([torch.nan]),
+            "is_trail_edge": torch.tensor(True, dtype=torch.bool),
+            "force_sign": torch.tensor(-1.0, dtype=torch.float),
+        }
+    
+    def sample_input(self, **kwargs):
+        """
+        No sampling required
+        """
+        return kwargs
+    
+    def validate_input(self, **kwargs):
+        """
+        No validation required
+        """
+        pass
+
+    def generate(self, 
+                n_nodes : int,
+                nodal_load : list,
+                trail_element_length : float,
+                origin_node_load : list,
+                ):
+        # Initialize graph
+        graph = StructData(
+                node_attrs=self.node_attrs,
+                edge_attrs=self.edge_attrs,
+                default_attrs=self.default_attrs,
+            )
+        
+        # Parse data as numpy arrays
+        origin_node_coords = np.zeros(3)
+        nodal_load = np.array(nodal_load)
+        origin_node_load = np.array(origin_node_load)
+        support_condition = torch.tensor([False, False, False])
+        
+        # Add origin nodes
+        graph.add_node(
+            f"node_0",
+            coords=origin_node_coords,
+            is_origin_node=torch.tensor(True),
+            sequence=torch.tensor(0),
+            load=nodal_load + origin_node_load,
+        )
+        for j in range(1,n_nodes-1):
+
+            # Add node
+            graph.add_node(
+                f"node_{j}",
+                is_origin_node=torch.tensor(False),
+                sequence=torch.tensor(j),
+                load=nodal_load,
+                support_condition=support_condition,
+            )
+            # Add new edge with the created node and its previous node
+            graph.add_edge(
+                f"node_{j-1}",
+                f"node_{j}",
+                is_trail_edge=torch.tensor(True),
+                length=trail_element_length,
+                force_sign=torch.tensor(-1.0),
+            )
+        
+        # Add node
+        j = n_nodes - 1 
+        graph.add_node(
+            f"node_{j}",
+            is_origin_node=torch.tensor(False),
+            sequence=torch.tensor(j),
+            load=nodal_load,
+            support_condition=torch.tensor([True, True, True]),
+        )
+        # Add new edge with the created node and its previous node
+        graph.add_edge(
+            f"node_{j-1}",
+            f"node_{j}",
+            is_trail_edge=torch.tensor(True),
+            length=trail_element_length,
+            force_sign=torch.tensor(-1.0),
+        )
+    
+        return graph
