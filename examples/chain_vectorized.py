92b061bffd0eb6ab554a6f6afb1964cf13fbcf20
lazlo-bleker <lwbleker@gmail.com>
2026-06-04 12:44:25 +0200

    formatting

--- examples/chain_vectorized.py
+++ examples/chain_vectorized.py
@@ -3,48 +3,21 @@ from torch_structure.data.data import StructData
 import matplotlib.pyplot as plt
 
 
-node_attrs = {
-    "coords": torch.empty((0, 3), dtype=torch.float)
-}
-
-edge_attrs = {
-    "force": torch.empty((0), dtype=torch.long)
-}
-
-
-data = StructData(
-    node_attrs=node_attrs,
-    edge_attrs=edge_attrs
-)
+node_attrs = {"coords": torch.empty((0, 3), dtype=torch.float)}
+edge_attrs = {"force": torch.empty((0), dtype=torch.long)}
+data = StructData(node_attrs=node_attrs, edge_attrs=edge_attrs)
 
 def f(u):
-
-    height = (u**2)/10
-
-    return torch.hstack([u,torch.zeros_like(u),height])
-
+    height = (u**2) / 10
+    return torch.hstack([u, torch.zeros_like(u), height])
 
 def g(u_coord):
-
-    res = u_coord**2 
-    
+    res = u_coord**2
     return res
 
-
-node_attrs = {
-    "coords": f
-}
-
-
-edge_attrs = {
-    "force": g
-}
-
+node_attrs = {"coords": f}
+edge_attrs = {"force": g}
 data.add_chain(10, node_attrs=node_attrs, edge_attrs=edge_attrs)
 
-
-data.plot(load=True)
-
-
+data.plot()
 plt.show()
-
--- examples/grid_vectorized.py
+++ examples/grid_vectorized.py
@@ -5,64 +5,36 @@ import matplotlib.pyplot as plt
 
 node_attrs = {
     "coords": torch.empty((0, 3), dtype=torch.float),
-    "load" : torch.empty((0,3), dtype=torch.float)
+    "load": torch.empty((0, 3), dtype=torch.float),
 }
-
-edge_attrs = {
-    "force": torch.empty((0), dtype=torch.long)
-}
-
-default_attrs = {
-
-    "load" : 10*torch.ones((1,3), dtype=torch.float)
-
-}
-
+edge_attrs = {"force": torch.empty((0), dtype=torch.long)}
+default_attrs = {"load": 10 * torch.ones((1, 3), dtype=torch.float)}
 data = StructData(
-    node_attrs=node_attrs,
-    edge_attrs=edge_attrs,
-    default_attrs=default_attrs
+    node_attrs=node_attrs, edge_attrs=edge_attrs, default_attrs=default_attrs
 )
 
-def f(u,v):
+def f(u, v):
+    height = (u**2 + v**2) / 10
+    return torch.hstack([u, v, height])
 
-    height = (u**2+v**2)/10
-
-    return torch.hstack([u,v,height])
-
-
-def f_2(u,v):
-
+def f_2(u, v):
     return torch.hstack([u, v, torch.zeros_like(u)])
-    
-
-def g_2(u_ind, is_boundary):
-
-    res = torch.ones_like(u_ind)
-    res[is_boundary] = -10
-
-    return res
-
 
 def g(u_coord, v_coord):
-
     res = u_coord**2 + v_coord**2
-    
     return res
 
+def g_2(u_ind, is_boundary):
+    res = torch.ones_like(u_ind)
+    res[is_boundary] = -10
+    return res
 
-node_attrs = {
-    "coords": f
-}
+node_attrs = {"coords": f}
 
+edge_attrs = {"force": g}
 
-edge_attrs = {
-    "force": g
-}
-
-data.add_grid(7,7, node_attrs=node_attrs, edge_attrs=edge_attrs)
+data.add_grid(7, 7, node_attrs=node_attrs, edge_attrs=edge_attrs)
 
 data.plot(load=True)
 
 plt.show()
-
