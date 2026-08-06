87a4f4b32cfa46ebeb9adec281061e8ca547086e
DanielMezaZ <jesusdaniel.mezazeron@gmail.com>
2026-08-06 12:45:33 +0200

    WIP: partial implementation

--- src/torch_structure/mixins/__init__.py
+++ src/torch_structure/mixins/__init__.py
@@ -9,3 +9,7 @@ class TSMixin(CEMMixin, FDMMixin, LaplacianSmoothingMixin, PlotMixin, TNAMixin):
     pass
 
 __all__ = ["TSMixin", "CEMMixin", "FDMMixin", "LaplacianSmoothingMixin", "PlotMixin", "TNAMixin", "OverrideResolveMixin"]
+
+solver_names = [
+    m for m in dir(TSMixin) if callable(getattr(TSMixin, m)) and not m.startswith("__")
+]
