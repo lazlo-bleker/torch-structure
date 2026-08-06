6137bf3b11d8e5e3e68451419344610abf8c41c0
davidbohm <142240022+davidbohm@users.noreply.github.com>
2026-08-06 08:05:34 +0200

    Cylinder and sphere (#12)
    * cylinder and sphere
    
    * added deleted_edges method (#11)
    
    * fix dimension edge direction
    
    ---------
    
    Co-authored-by: David Böhm <david.boehm@ist.ac.at>
    Co-authored-by: Lazlo Bleker <94058519+lazlo-bleker@users.noreply.github.com>

---
+++ examples/cylinder_vectorized.py
@@ -0,0 +1,38 @@
+import torch
+from torch_structure.data.data import StructData
+import matplotlib.pyplot as plt
+N_SECTORS = 8
+N_RINGS = 5
+
+node_attrs = {
+    "coords": torch.empty((0, 3), dtype=torch.float)
+}
+
+edge_attrs = {
+    "force": torch.empty((0), dtype=torch.long)
+}
+
+data = StructData(
+    node_attrs=node_attrs,
+    edge_attrs=edge_attrs
+)
+
+def f(x_unit_coord, y_unit_coord, z_unit_coord):
+    return torch.hstack([x_unit_coord, y_unit_coord, z_unit_coord])
+
+def g(z_unit_coord):
+    return 1 + z_unit_coord * 15
+
+node_attrs = {
+    "coords": f
+}
+
+edge_attrs = {
+    "force": g
+}
+
+data.add_cylinder(N_SECTORS, N_RINGS, node_attrs=node_attrs, edge_attrs=edge_attrs)
+
+data.plot(load=True)
+
+plt.show()
---
+++ examples/sphere_vectorized.py
@@ -0,0 +1,37 @@
+import torch
+from torch_structure.data.data import StructData
+import matplotlib.pyplot as plt
+N_SECTORS = 10
+N_RINGS = 7
+NUM_EDGES = (2 * N_RINGS + 1) * N_SECTORS
+
+node_attrs = {
+    "coords": torch.empty((0, 3), dtype=torch.float)
+}
+
+edge_attrs = {
+    "force": torch.empty((0), dtype=torch.long)
+}
+
+data = StructData(
+    node_attrs=node_attrs,
+    edge_attrs=edge_attrs
+)
+
+def f(x_unit_coord, y_unit_coord, z_unit_coord):
+    return torch.hstack([x_unit_coord, y_unit_coord, z_unit_coord])
+
+
+node_attrs = {
+    "coords": f
+}
+
+edge_attrs = {
+    "force": torch.ones(NUM_EDGES, dtype=torch.float)
+}
+
+data.add_sphere(N_SECTORS, N_RINGS, node_attrs=node_attrs, edge_attrs=edge_attrs)
+
+data.plot(load=True)
+
+plt.show()
